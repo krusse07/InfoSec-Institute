58d218b7c7efbadf71e3a31693d1e51dd182ca15
krusse07 <42067799+krusse07@users.noreply.github.com>
2020-04-12 00:41:48 -0400

    updated captcha_generator.php
    Wrote the code to generate captcha images. $x = number of images. captchas will be in the tmp folder.

--- Captcha Breaking Bot/captcha_generator.php
+++ Captcha Breaking Bot/captcha_generator.php
@@ -1,1 +1,23 @@
+<?php
 
+/**
+* Plugin Name: CAPTCHA Generator
+* Plugin URI: 
+* Description: This WordPress plugin collects CAPTCHA images
+* Version 1.0.0
+* Author: Kiel Russell
+*
+*
+*/
+$root = realpath($_SERVER["DOCUMENT_ROOT]);
+$simple_captcha_plugin_path = $root/CAPTCHA/wp-content/plugins/really-simple-captcha/really-simple-captcha.php";
+include $simple_capture_plugin_path;
+$pluggable = "$root/CAPTCHA/wp-includes/pluggable.php"; 
+include $pluggable;
+//generate 1000 captcha images x = num of images
+for ($x = 0;  $x <= 1000; $x++)    {                      
+$captcha_instance = new ReallySimpleCaptcha();
+$word = $captcha_instance->generate_random_word();         
+$captcha_instance->generate_image($word, $word);    
+}
+?>
